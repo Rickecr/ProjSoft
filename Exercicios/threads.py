c62c947bab4f239ee389cba353c843a3bc4bbafd
Rickecr <richelton14@gmail.com>
2019-03-21 14:21:17 -0300

    Estudos sobre o básico de threads concluído. Exercicio sobre threads concluído.

---
+++ Exercicios/threads.py
@@ -0,0 +1,31 @@
+from threading import Thread
+import time
+
+class Contador(Thread):
+    def __init__(self, nome, segundos, intervalo):
+        super(Contador, self).__init__()
+        self.nome = nome
+        self.segundos = segundos
+        self.intervalo = intervalo
+
+    def run(self):
+        while self.segundos:
+            print("%s: %s" % (self.nome, self.segundos))
+            time.sleep(self.intervalo / 1000.0)
+            self.segundos -= 1
+
+print('Iniciando contador 1 ("5 Segundos")')
+thread1 = Contador("5 Segundos", 5, 1000)
+
+print('Iniciando contador 2 ("A")')
+thread2 = Contador("A", 15, 300)
+
+print("Iniciando thread1")
+thread1.start()
+
+time.sleep(5)
+
+print("Iniciando thread2")
+thread2.start()
+
+print('Fim')
