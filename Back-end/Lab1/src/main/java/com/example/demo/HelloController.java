68fa075f3c870e290ef13ada872589c849d106ba
Rickecr <richelton14@gmail.com>
2019-05-19 20:57:28 -0300

    Primeira rota com parâmetros concluida

--- Back-end/Lab1/src/main/java/com/example/demo/HelloController.java
+++ Back-end/Lab1/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String hello() {
-		return "Olá, mundo!";
-	}
-	
+    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 }
