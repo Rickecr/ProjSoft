a7cc0399068db4faf1fdc9ae67b1f0460df0b369
Rickecr <richelton14@gmail.com>
2019-05-19 21:55:48 -0300

    Adicionando saudações

--- Back-end/Lab1/src/main/java/com/example/demo/HelloController.java
+++ Back-end/Lab1/src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -11,6 +14,18 @@ public class HelloController {
 	@GetMapping("/hello")
     public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        String saudacao = "";
+        LocalTime localTime = LocalTime.now();
+        int horas = localTime.getHour();
+        if (horas <= 12) {
+        	saudacao = "bom dia";
+        } else if (horas > 12 && horas < 18) {
+        	saudacao = "boa tarde";
+        } else if (horas >= 18) {
+        	saudacao = "boa noite";
+        }
+        model.addAttribute("saudacao", saudacao);
+        
         return "hello";
     }
 }
