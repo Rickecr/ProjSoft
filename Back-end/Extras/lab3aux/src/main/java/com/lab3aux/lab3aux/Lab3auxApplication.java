046f0815cd94af12a30c83efc008044d4409dac5
Rickecr <richelton14@gmail.com>
2019-06-01 21:41:41 -0300

    Estudando autorização com JWT e Spring Boot Security

---
+++ Back-end/Extras/lab3aux/src/main/java/com/lab3aux/lab3aux/Lab3auxApplication.java
@@ -0,0 +1,13 @@
+package com.lab3aux.lab3aux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3auxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3auxApplication.class, args);
+	}
+
+}
---
+++ Back-end/Extras/lab3aux/src/main/java/com/lab3aux/lab3aux/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.lab3aux.lab3aux.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping(value = "/users")
+	@ResponseBody
+	public String getUsers() {
+		return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+		           "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+	}
+	
+}
