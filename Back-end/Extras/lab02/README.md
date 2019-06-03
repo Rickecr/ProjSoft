e36bbc19ed0fb722a3931c3807cea26289481a0e
Rich Elton <richelton14@gmail.com>
2019-06-03 15:48:44 -0300

    lab02 com autenticação: Extra

---
+++ Back-end/Extras/lab02/README.md
@@ -0,0 +1,25 @@
+# Laboratório 2 - Lab CRUD de produtos com REST, Spring Boot e JPA/hibernate
+
+> Ter um contato inicial com o framework de desenvolvimento que iremos adotar na disciplina (spring boot) para desenvolver uma aplicação muito simples.
+
+Vamos criar um CRUD de um produto, utilizando o spring boot e o hibernate. Vamos utilizar o JPA (Java Persistence API) como interface para utilizar o hibernate e por consequência conectar o banco e a aplicação. O JPA é uma interface que serve de abstração para que você possa utilizar diversos ORMs. ORM (Object Relational Mapper) é o serviço que mapeia objetos para dados do banco de dados e vice versa. Uma ferramenta ORM muito usada é o hibernate, mas existem diversas outras.
+
+Ao final do lab você deve ter uma api que tem serviços para criar, atualizar, deletar e buscar um produto genérico. Você deve ter integração com algum banco de dados e você terá como opcional tratamento de exceptions que irá auxiliá-lo no seu projeto.
+
+- [X] Criando o Model Produto.
+- [X] Criando o DAO de Produto.
+- [X] Criando o Service de Produto.
+- [X] Criando o Controller de Produtos:
+  - [X] Create:
+    - [X] Cadastrar um novo produto(POST).
+  - [X] Read:
+    - [X] Buscar um produto especificado pelo ID(GET).
+    - [X] Retornar todos os produtos cadastrados(GET). (EXTRA)
+  - [X] Update:
+    - [X] Atualizar um produto por completo(PUT).
+    - [X] Atualizar um produto parcialmente(PATCH). (EXTRA)
+  - [X] Delete:
+    - [X] Deletar um produto pelo ID(DELETE).
+- [X] Conexão com o banco de dados:
+  - [X] H2
+- [X] Testando com o Postman.
---
+++ Back-end/Extras/lab02/src/main/java/com/psoft/lab02/controller/ProductController.java
@@ -0,0 +1,101 @@
+package com.psoft.lab02.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.psoft.lab02.Exception.Product.ProductNotFoundException;
+import com.psoft.lab02.model.Product;
+import com.psoft.lab02.service.ProductService;
+
+@RestController
+@RequestMapping({"/v1/products"})
+public class ProductController {
+
+	private ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@GetMapping(value = "/{id}")
+	@ResponseBody
+	public ResponseEntity<Product> findyById(@PathVariable long id) {
+		Product product = this.productService.findyById(id);
+		
+		if (product == null) {
+			throw new ProductNotFoundException("Produto não existe!");
+		}
+		
+		return new ResponseEntity<Product>(product, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/")
+	public ResponseEntity<List<Product>> getAll() {
+		try {
+			return new ResponseEntity<List<Product>>( this.productService.getAll(), HttpStatus.OK);
+		} catch (Exception e) {
+			throw new InternalError("Something went wrong");
+		}
+	}
+	
+	@PostMapping(value = "/")
+	@ResponseBody
+	public ResponseEntity<Product> create(@RequestBody Product product) {
+		Product newProduct = this.productService.create(product);
+		
+		if (newProduct == null) {
+			throw  new InternalError("Something went wrong");
+		}
+		
+		return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity delete(@PathVariable long id) {
+		try {
+			this.productService.delete(id);
+			return new ResponseEntity(HttpStatus.OK);
+		} catch (Exception e) {
+			throw new InternalError("Something went wrong");
+		}
+	}
+	
+	@PutMapping(value = "/")
+	@ResponseBody
+	public ResponseEntity<Product> update(@RequestBody Product product) {
+		try {
+			Product updated = this.productService.update(product);
+			return new ResponseEntity<Product>(updated, HttpStatus.OK);
+		} catch (Exception e) {
+			throw new InternalError("Something went wrong");
+		}
+	}
+	
+	@PatchMapping(value = "/{id}")
+	@ResponseBody
+	public ResponseEntity<Product> updatePartial(
+			@PathVariable long id,
+			@RequestBody Product product) {
+		try {
+			product.setId(id);
+			Product product2 = this.productService.updatePartial(product);
+			return new ResponseEntity<Product>(product2, HttpStatus.OK);
+		} catch (Exception e) {
+			throw new InternalError("Something went wrong");
+		}
+	}
+	
+}
